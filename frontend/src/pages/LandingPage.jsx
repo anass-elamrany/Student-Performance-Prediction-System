c630e94a6ec4c170e50f954d52d95d6574c9ce73
ansss22 <elamranyanass22@gmail.com>
2025-12-17 21:19:41 +0100

    Remove Testimonials section

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -2,7 +2,6 @@ import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import RoleShowcase from "../components/RoleShowcase";
 import Stats from "../components/Stats";
-import Testimonials from "../components/Testimonials";
 import Partners from "../components/Partners";
 import CallToAction from "../components/CallToAction";
 import FAQ from "../components/FAQ";
@@ -17,7 +16,6 @@ const LandingPage = () => {
         <Partners />
         <Stats />
         <RoleShowcase />
-        <Testimonials />
         <FAQ />
         <CallToAction />
       </main>
